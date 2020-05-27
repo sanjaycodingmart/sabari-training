33f035af5aa1a6164b44028217d9474172016592
Sabari-rs1 <sabari@codingmart.com>
2020-05-27 13:48:27 +0530

    second commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,8 @@ export default class App extends React.Component
     super();
     this.state={
       userinput:'',
-      list: []
+      list: [],
+      key: 0
     }
   }
 
@@ -23,8 +24,26 @@ export default class App extends React.Component
     let temp=this.state.list;
     temp.push(item);
     this.setState({list:temp});
+   
+
+    //increment key
+    var count=this.state.key;
+    count=count+1;
+    this.setState({key:count});
+    console.log("key"+this.state.key);  //why is the state.key not incremented like the count bro?
   }
 
+
+
+  removeItem(e)
+  {
+    //var checkBox = document.getElementById("myCheck");
+    //if (checkBox.checked === true){
+      //console.log(value);
+      
+    //}
+    console.log(e.target.getAttribute("newid")); //unable to get the value of newid of previous items from checkbox bro
+  }
 
   render()
   {
@@ -34,7 +53,11 @@ export default class App extends React.Component
       <button onClick={()=>this.addList(this.state.userinput)}>ADD</button>
 
       <div>
-        {this.state.list.map((val)=><li>{val}</li>)}
+        {this.state.list.map((val)=>
+        <div>
+          <li>{val}<input type="checkbox" id="myCheck" newid={this.state.key} onChange={(e)=>this.removeItem(e)}></input></li>
+        </div>)
+        }
       </div>
     </div>);
   }
