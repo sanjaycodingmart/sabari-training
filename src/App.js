055d05cc97eeab492416c35e837d7142e1146679
Sabari-rs1 <sabari@codingmart.com>
2020-06-03 11:39:02 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+    constructor()
+    {
+        super();
+        this.state = {
+            url : []
+        }
+        this.observer = null;
+        
+    }
+    
+    ref = React.createRef();
+   
+
+    componentDidMount()
+    {
+        this.observer = new IntersectionObserver(([e]) =>{
+        
+        if (e.intersectionRatio>0)
+        {
+            this.getNewImage();     
+        }},
+        {
+            threshold : 1
+        });
+        this.observer.observe(this.ref.current) ;
+    }
+
+
+    getNewImage()
+    {
+     const newimgurl="https://picsum.photos/200/300";           
+    const arr = [...this.state.url,newimgurl]
+    this.setState({url : arr});   
+    }
+
+
+    render()
+    {
+        return (
+            <div>
+            <h1>
+                Infinite Scroll using IO
+            </h1>
+            <div>
+             {
+                this.state.url.map((url,index) =>
+                {
+                return (<div key={ index } newref={(input)=>{this.divitem=input}}>
+                    <img src = { url }  style={{height:"800px",width:"100%" }} alt = "img not loaded"/>
+                </div>)
+                }
+                )
+            }
+            </div>
+            <footer ref={this.ref}>
+                Loading
+            </footer>
+            </div>
+        )
+    }
+
 }
 
 export default App;
