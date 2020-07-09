c2ba49d6a859ed5782c77ff22b6d5ee353e4afc8
sabari RS <65396671+Sabari-rs1@users.noreply.github.com>
2020-07-09 13:13:25 +0530

    Add files via upload

--- middleware/adminDb.js
+++ middleware/adminDb.js
@@ -1,27 +1,4 @@
-const User=require('../models/User')
-
-function adminDb() {
-
-const data=[]
-
-User.findAll({
-  where: {
-      email: 'sa2@'  
-  },
-  attributes: ['id', 'name','email','password','role'], 
-}).then((persons)=>{
-  try{
-    data.push(persons)
-    console.log('admin data fethed'+persons[0].dataValues)
-    return data
-  }catch(err)
-  {
-    console.log("no such user found in db so admin cant access it")
-    return null
-  }
-})
-
-
+function makeAdmin(event)
+{
+  console.log("logg")
 }
-
-module.exports=adminDb;
